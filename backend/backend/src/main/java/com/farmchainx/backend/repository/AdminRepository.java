7cd2152ab5f3b635ff85810cb179377ee3e9ffe6
Kamil Ahmed Shaik <shaik.kamilahmed123@gmail.com>
2026-01-22 13:14:01 +0530

    Delete backend/backend directory

--- backend/backend/src/main/java/com/farmchainx/backend/repository/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.farmchainx.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.farmchainx.backend.entity.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-}
--- backend/backend/src/main/java/com/farmchainx/backend/repository/RetailerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.farmchainx.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.farmchainx.backend.entity.Retailer;
-
-public interface RetailerRepository extends JpaRepository<Retailer, Long> {
-}
